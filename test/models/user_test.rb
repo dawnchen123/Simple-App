e79b5f1436d7f21b8fd78edd1edec89f6c007d39
dawn <1142144213@qq.com>
2016-01-23 13:03:56 +0800

    Finish login and logout

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,4 +1,12 @@
-class CreateUsers < ActiveRecord::Migration
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+
+  def setup
+    @user = User.new(name: "dawn", email: "1142144213@qq.com",
+                     password: "tobeno.1", password_confirmation: "tobeno.1")
+  end
+
   def change
     create_table :users do |t|
       t.string :name
@@ -6,5 +14,9 @@ class CreateUsers < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+  end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
   end
 end
