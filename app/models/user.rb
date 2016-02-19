c56cb06991322a45cccbd907b25de505abc1c50d
dawn <1142144213@qq.com>
2016-02-19 09:39:09 +0800

    redirected

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
   validates(:name, presence: true, length: { maximum: 50} )
@@ -51,6 +51,24 @@ class User < ActiveRecord::Base
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
+
+  #设置密码重设相关的属性
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  #发送密码重设邮件
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  #如果密码重设超时失效， 返回true
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   private
 
     #把电子邮件地址转换成小写
@@ -63,5 +81,6 @@ class User < ActiveRecord::Base
       self.activation_token = User.new_token
       self.activation_digest = User.digest(activation_token)
     end
+
 
 end
---
+++ test/integration/password_resets_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class PasswordResetsTest < ActionDispatch::IntegrationTest
+
+  def setup
+    ActionMailer::Base.deliveries.clear
+    @user = users(:michael)
+  end
+
+  test "password resets" do
+    get new_password_reset_path
+    assert_template 'password_resets/new'
+    #电子邮件地址无效
+  end
+
+end
